9f0d7ca01ee412816c3bc307c473065533c27b6d
alpkumbasar <alp.kumbasar@kuartis.com>
2021-06-22 16:15:42 +0300

    only resize layer commented

--- yolov5.cpp
+++ yolov5.cpp
@@ -76,37 +76,39 @@
         // upsample11->setResizeMode(ResizeMode::kNEAREST);
         // upsample11->setOutputDimensions(bottleneck_csp6->getOutput(0)->getDimensions());
 
-        // ITensor *inputTensors12[] = {upsample11->getOutput(0), bottleneck_csp6->getOutput(0)};
-        // auto cat12 = network->addConcatenation(inputTensors12, 2);
-        // auto bottleneck_csp13 = C3(network, weightMap, *cat12->getOutput(0), get_width(1024, gw), get_width(512, gw), get_depth(3, gd), false, 1, 0.5, "model.13");
-        // auto conv14 = convBlock(network, weightMap, *bottleneck_csp13->getOutput(0), get_width(256, gw), 1, 1, 1, "model.14");
+        auto upsample11 = resizeNearest()
+
+        ITensor *inputTensors12[] = {upsample11->getOutput(0), bottleneck_csp6->getOutput(0)};
+        auto cat12 = network->addConcatenation(inputTensors12, 2);
+        auto bottleneck_csp13 = C3(network, weightMap, *cat12->getOutput(0), get_width(1024, gw), get_width(512, gw), get_depth(3, gd), false, 1, 0.5, "model.13");
+        auto conv14 = convBlock(network, weightMap, *bottleneck_csp13->getOutput(0), get_width(256, gw), 1, 1, 1, "model.14");
 
         // auto upsample15 = network->addResize(*conv14->getOutput(0));
         // assert(upsample15);
         // upsample15->setResizeMode(ResizeMode::kNEAREST);
         // upsample15->setOutputDimensions(bottleneck_csp4->getOutput(0)->getDimensions());
 
-        // ITensor *inputTensors16[] = {upsample15->getOutput(0), bottleneck_csp4->getOutput(0)};
-        // auto cat16 = network->addConcatenation(inputTensors16, 2);
+        ITensor *inputTensors16[] = {upsample15->getOutput(0), bottleneck_csp4->getOutput(0)};
+        auto cat16 = network->addConcatenation(inputTensors16, 2);
 
-        // auto bottleneck_csp17 = C3(network, weightMap, *cat16->getOutput(0), get_width(512, gw), get_width(256, gw), get_depth(3, gd), false, 1, 0.5, "model.17");
+        auto bottleneck_csp17 = C3(network, weightMap, *cat16->getOutput(0), get_width(512, gw), get_width(256, gw), get_depth(3, gd), false, 1, 0.5, "model.17");
 
         // /* ------ detect ------ */
-        // IConvolutionLayer *det0 = network->addConvolution(*bottleneck_csp17->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.24.m.0.weight"], weightMap["model.24.m.0.bias"]);
-        // auto conv18 = convBlock(network, weightMap, *bottleneck_csp17->getOutput(0), get_width(256, gw), 3, 2, 1, "model.18");
-        // ITensor *inputTensors19[] = {conv18->getOutput(0), conv14->getOutput(0)};
-        // auto cat19 = network->addConcatenation(inputTensors19, 2);
-        // auto bottleneck_csp20 = C3(network, weightMap, *cat19->getOutput(0), get_width(512, gw), get_width(512, gw), get_depth(3, gd), false, 1, 0.5, "model.20");
-        // IConvolutionLayer *det1 = network->addConvolution(*bottleneck_csp20->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.24.m.1.weight"], weightMap["model.24.m.1.bias"]);
-        // auto conv21 = convBlock(network, weightMap, *bottleneck_csp20->getOutput(0), get_width(512, gw), 3, 2, 1, "model.21");
-        // ITensor *inputTensors22[] = {conv21->getOutput(0), conv10->getOutput(0)};
-        // auto cat22 = network->addConcatenation(inputTensors22, 2);
-        // auto bottleneck_csp23 = C3(network, weightMap, *cat22->getOutput(0), get_width(1024, gw), get_width(1024, gw), get_depth(3, gd), false, 1, 0.5, "model.23");
-        // IConvolutionLayer *det2 = network->addConvolution(*bottleneck_csp23->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.24.m.2.weight"], weightMap["model.24.m.2.bias"]);
+        IConvolutionLayer *det0 = network->addConvolution(*bottleneck_csp17->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.24.m.0.weight"], weightMap["model.24.m.0.bias"]);
+        auto conv18 = convBlock(network, weightMap, *bottleneck_csp17->getOutput(0), get_width(256, gw), 3, 2, 1, "model.18");
+        ITensor *inputTensors19[] = {conv18->getOutput(0), conv14->getOutput(0)};
+        auto cat19 = network->addConcatenation(inputTensors19, 2);
+        auto bottleneck_csp20 = C3(network, weightMap, *cat19->getOutput(0), get_width(512, gw), get_width(512, gw), get_depth(3, gd), false, 1, 0.5, "model.20");
+        IConvolutionLayer *det1 = network->addConvolution(*bottleneck_csp20->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.24.m.1.weight"], weightMap["model.24.m.1.bias"]);
+        auto conv21 = convBlock(network, weightMap, *bottleneck_csp20->getOutput(0), get_width(512, gw), 3, 2, 1, "model.21");
+        ITensor *inputTensors22[] = {conv21->getOutput(0), conv10->getOutput(0)};
+        auto cat22 = network->addConcatenation(inputTensors22, 2);
+        auto bottleneck_csp23 = C3(network, weightMap, *cat22->getOutput(0), get_width(1024, gw), get_width(1024, gw), get_depth(3, gd), false, 1, 0.5, "model.23");
+        IConvolutionLayer *det2 = network->addConvolution(*bottleneck_csp23->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.24.m.2.weight"], weightMap["model.24.m.2.bias"]);
 
-        // auto yolo = addYoLoLayer(network, weightMap, "model.24", std::vector<IConvolutionLayer *>{det0, det1, det2});
-        // yolo->getOutput(0)->setName(OUTPUT_BLOB_NAME);
-        // network->markOutput(*yolo->getOutput(0));
+        auto yolo = addYoLoLayer(network, weightMap, "model.24", std::vector<IConvolutionLayer *>{det0, det1, det2});
+        yolo->getOutput(0)->setName(OUTPUT_BLOB_NAME);
+        network->markOutput(*yolo->getOutput(0));
 
         // Build engine
         builder->setMaxBatchSize(maxBatchSize);
